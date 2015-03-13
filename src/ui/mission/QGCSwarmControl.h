5cef53d4aa1379c0c451340410267f2666e00109
ndousse <nicolas.dousse@epfl.ch>
2015-03-13 17:56:49 +0100

    Replaced tabs by 4 spaces

--- src/ui/mission/QGCSwarmControl.h
+++ src/ui/mission/QGCSwarmControl.h
@@ -24,46 +24,46 @@ public:
 
 public slots:
 
-	void textMessageReceived(UASInterface* uas, QString message);
-	void textEmit(int uasid, int component, int severity, QString message);
+    void textMessageReceived(UASInterface* uas, QString message);
+    void textEmit(int uasid, int component, int severity, QString message);
 
 private slots:
     void continueAllButton_clicked();
-	void Return2startButton_clicked();
-	void launchScenario_clicked();
-	void autoLanding_clicked();
-	void autoTakeoff_clicked();
+    void Return2startButton_clicked();
+    void launchScenario_clicked();
+    void autoLanding_clicked();
+    void autoTakeoff_clicked();
 
-	void startLogging_clicked();
-	void stopLogging_clicked();
+    void startLogging_clicked();
+    void stopLogging_clicked();
 
-	void UASCreated(UASInterface* uas);
-	void RemoveUAS(UASInterface* uas);
-	void ListWidgetClicked(QListWidgetItem* item);
+    void UASCreated(UASInterface* uas);
+    void RemoveUAS(UASInterface* uas);
+    void ListWidgetClicked(QListWidgetItem* item);
 
-	void newActiveUAS(UASInterface* uas);
+    void newActiveUAS(UASInterface* uas);
 
-	void refreshView();
+    void refreshView();
 
-	void setParameters_clicked();
+    void setParameters_clicked();
 
-	void startButton_clicked();
-	void stopButton_clicked();
+    void startButton_clicked();
+    void stopButton_clicked();
 
-	void armButton_clicked();
-	void disarmButton_clicked();
+    void armButton_clicked();
+    void disarmButton_clicked();
 
-	void selectButton_clicked();
+    void selectButton_clicked();
 
-	void remoteItem_clicked(QListWidgetItem* item);
+    void remoteItem_clicked(QListWidgetItem* item);
 
-	void remoteButton_clicked();
+    void remoteButton_clicked();
 
-	void setMode(int mode);
+    void setMode(int mode);
 
-	void sendNewHomePosition();
+    void sendNewHomePosition();
 
-	void strategyLaunchClicked();
+    void strategyLaunchClicked();
 
 private:
     Ui::QGCSwarmControl *ui;
@@ -73,29 +73,29 @@ private:
     int wptReachedCnt;
 
 protected:
-	MAVLinkProtocol* mavlink;     ///< Reference to the MAVLink instance
-	UASInterface *uas;
-	QList<UASInterface*> UASlist;
+    MAVLinkProtocol* mavlink;     ///< Reference to the MAVLink instance
+    UASInterface *uas;
+    QList<UASInterface*> UASlist;
 
-	QMap<UASInterface*,QListWidgetItem*> uasToItemMapping;
-	QMap<QListWidgetItem*,UASInterface*> itemToUasMapping;
+    QMap<UASInterface*,QListWidgetItem*> uasToItemMapping;
+    QMap<QListWidgetItem*,UASInterface*> itemToUasMapping;
 
-	QMap<UASInterface*,QListWidgetItem*> uasToItemRemote;
-	QMap<QListWidgetItem*,UASInterface*> itemToUasRemote;	
+    QMap<UASInterface*,QListWidgetItem*> uasToItemRemote;
+    QMap<QListWidgetItem*,UASInterface*> itemToUasRemote;    
 
-	UASInterface *uas_previous;
+    UASInterface *uas_previous;
 
-	QTimer updateTimer; ///< Only update at 1 Hz to not overload the GUI
-	static const unsigned int updateInterval; ///< The update interval of the refresh function
+    QTimer updateTimer; ///< Only update at 1 Hz to not overload the GUI
+    static const unsigned int updateInterval; ///< The update interval of the refresh function
 
-	bool all_selected;
+    bool all_selected;
 
-	bool mode_init;
+    bool mode_init;
     int modeIdx;                    ///< Current uas mode index
- 	QList<AutoPilotPluginManager::FullMode_t>  _modeList;  ///< Mode list for the current UAS
+     QList<AutoPilotPluginManager::FullMode_t>  _modeList;  ///< Mode list for the current UAS
 
 signals:
-	void uasTextReceived(UASInterface* uas, QString message);
+    void uasTextReceived(UASInterface* uas, QString message);
 
 };
 
